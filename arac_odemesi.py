0f49fca9fa5c00c82a3d27892cd5ff8fb7924167
seko <buboylesusoyle@gmail.com>
2022-01-09 01:59:42 +0300

    ödev3

---
+++ arac_odemesi.py
@@ -0,0 +1,10 @@
+a= float(input("Toplam tutarı hesaplamak için bilgileri giriniz.\nKilometre başına yakılan ücret: "))
+b= float(input("Kaç kilometredir yoldasınız: "))
+print("Ödemeniz gereken toplam ücret {} TL'dir".format(a*b))
+
+
+
+
+
+
+
