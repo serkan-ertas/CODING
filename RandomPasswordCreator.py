fe9b9e08880291a9816120f67d831214655cc0ef
Serkan <68593142+buboylesusoyle@users.noreply.github.com>
2022-06-17 01:30:04 +0300

    Update RandomPasswordCreator.py
    imported string module

--- RandomPasswordCreator.py
+++ RandomPasswordCreator.py
@@ -1,7 +1,8 @@
 import random
+import string
 
 def password(length):    ##you should provide a length to the code
     pw = str()
     for i in range(length):
-        pw += random.choice("qwertyuopasdfghjklizxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM.12345678901234567890*/")
-    print(pw)
+        pw += random.choice(string.ascii_letters + string.digits*2 + string.punctuation[:10])
+    return(pw)
