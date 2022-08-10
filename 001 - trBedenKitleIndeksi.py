317acb29ec8aaa86230d6cf79e3f145636f95d8c
Serkan <buboylesusoyle@gmail.com>
2022-08-10 18:45:54 +0300

    edited

---
+++ 001 - trBedenKitleIndeksi.py
@@ -0,0 +1,6 @@
+print("Beden Kitle İndeksinizi hesaplamamız için lütfen bilgilerinizi giriniz.")
+kilo= float(input("Kilonuz= "))
+boy= float(input("Boyunuz(m)= "))
+a= str(kilo/boy/boy)
+print("Beden Kitle İndeksiniz: {}".format(a[:4]))
+
